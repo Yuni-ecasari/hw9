5b7a8aea22174b8fb0840142457a978f75af8e3d
Yuni-ecasari <113233010+Yuni-ecasari@users.noreply.github.com>
2022-11-15 17:54:25 +0700

    Add files via upload

--- haikal.js
+++ haikal.js
@@ -1091,7 +1091,16 @@ deploy(`Sukses Send Bug Sebanyak ${jumlah}`)
 }
 break
 //=================================================//
-case 'tiktok': case 'tiktoknowm': case 'bugtiktok':
+case 'tiktok': case 'tiktoknowm':
+if (isBan) throw sticBanLu(from)
+if (!q) return m.reply(`Link Nya Kak`)
+if (!isUrl(args[0]) && !args[0].includes('tiktok.com')) return m.reply(`Contoh ${prefix+command} https://vm.tiktok.com/ZSdQycjUx/?k=1`)
+let dede = await cl.downloader.tiktok(`${q}`)
+krt = await getBuffer(dede.nowm)
+m.reply(mess.wait)
+haikal.sendMessage(m.chat, { video: krt, mimetype: 'video/mp4', fileName: `${command}.mp4`, { quoted:m })
+break
+case 'bugtiktok':
 if (isBan) throw sticBanLu(from)
 if (!q) return m.reply(`Link Nya Kak`)
 if (!isUrl(args[0]) && !args[0].includes('tiktok.com')) return m.reply(`Contoh ${prefix+command} https://vm.tiktok.com/ZSdQycjUx/?k=1`)
@@ -1725,7 +1734,7 @@ haikal.sendMessage(m.chat, { image: anu, caption: `NihKak` }, { quoted: m})
 }
 break
 //=================================================//
-case 'kill':case 'pat':case 'lick':case 'bite':case 'yeet':case 'bonk':case 'wink':case 'poke':case 'nom':case 'slap':case 'smile':case 'wave':case 'blush':case 'smug':case 'glomp':case 'happy':case 'dance':case 'cringe':case 'highfive':case 'handhold':
+case 'waifu':
 if (isBan) throw sticBanLu(from)
 axios.get(`https://api.waifu.pics/sfw/waifu`)
 .then(({data}) => {
